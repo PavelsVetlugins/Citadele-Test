7d5c393499eeb53bc0aeee18c317ec42545610c6
PavelsVetlugins <pavelvetl92@gmail.com>
2022-11-16 10:04:26 +0200

    initial

---
+++ Citadele-test/Citadele_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Citadele_testApp.swift
+//  Citadele-test
+//
+//  Created by Pavels Vetlugins on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Citadele_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
