da745f2a5c14a6c3efe726677ba72d1805d2aef0
PavelsVetlugins <pavelvetl92@gmail.com>
2022-11-17 18:06:25 +0200

    Tests

--- Citadele-testTests/CurrencyConverterVMTest.swift
+++ Citadele-testTests/CurrencyConverterVMTest.swift
@@ -6,13 +6,48 @@
 //
 
 @testable import Citadele_test
+import Combine
 import XCTest
 
 final class CurrencyConverterVMTest: XCTestCase {
-    func testAAA() {
+    func testSelectingCurrrenctWillSetChildRate() throws {
         let container = DIContainerMock.previewMock() // can be used specific mock, but this one fits our needs for now
-
         let converterVM = CurrencyConverterVM(diContainer: container)
-//        converterVM.
+
+        let rate = Rate(currency: "USD", description: "Dollar", sellRate: "1.4", buyRate: "1.6", sellTransfer: "1.45", buyTransfer: "1.55")
+        let currency = Currency(id: "EUR", description: "Euro", reverseUsdQuot: false, rates: [rate])
+
+        converterVM.selectedCurrency = currency
+
+        let result = try awaitPublisher(converterVM.$selectedRate.dropFirst())
+        XCTAssertEqual(result.id, rate.id)
+    }
+
+    func testInputFieldCleansing() throws {
+        let container = DIContainerMock.previewMock()
+        let converterVM = CurrencyConverterVM(diContainer: container)
+
+        converterVM.isSellingFieldEditing.send(true)
+        converterVM.isBuyingFieldEditing.send(false)
+
+        converterVM.sellingCurrencyValue = "10"
+        var result = try awaitPublisher(converterVM.$sellingCurrencyValue)
+        XCTAssertEqual(result, "10")
+
+        converterVM.sellingCurrencyValue = "10.01"
+        result = try awaitPublisher(converterVM.$sellingCurrencyValue)
+        XCTAssertEqual(result, "10.01")
+
+        converterVM.sellingCurrencyValue = "aaa"
+        result = try awaitPublisher(converterVM.$sellingCurrencyValue.dropFirst())
+        XCTAssertEqual(result, "")
+
+        converterVM.sellingCurrencyValue = "10.00.0"
+        result = try awaitPublisher(converterVM.$sellingCurrencyValue.dropFirst())
+        XCTAssertEqual(result, "10.000")
+
+        converterVM.sellingCurrencyValue = ".10"
+        result = try awaitPublisher(converterVM.$sellingCurrencyValue.dropFirst())
+        XCTAssertEqual(result, "10")
     }
 }
---
+++ Citadele-testTests/RateCalculatorTest.swift
@@ -0,0 +1,33 @@
+//
+//  RateCalculatorTest.swift
+//  Citadele-testTests
+//
+//  Created by Pavels Vetlugins on 17/11/2022.
+//
+
+@testable import Citadele_test
+import XCTest
+
+final class RateCalculatorTest: XCTestCase {
+    func testAAA() throws {
+        var calculator = RateCalculator(sellRate: 1.485, buyRate: 1.585, isSelling: false, isReverseUsdQuot: false, isUSDCurrency: false)
+        var result = calculator.calculateRate(amount: 10)
+        XCTAssertEqual(result, "15.85")
+
+        calculator = RateCalculator(sellRate: 1.485, buyRate: 1.585, isSelling: true, isReverseUsdQuot: true, isUSDCurrency: true)
+        result = calculator.calculateRate(amount: 10)
+        XCTAssertEqual(result, "15.85")
+
+        calculator = RateCalculator(sellRate: 1.485, buyRate: 1.585, isSelling: true, isReverseUsdQuot: true, isUSDCurrency: false)
+        result = calculator.calculateRate(amount: 10)
+        XCTAssertEqual(result, "6.73")
+
+        calculator = RateCalculator(sellRate: 1.485, buyRate: 1.585, isSelling: true, isReverseUsdQuot: false, isUSDCurrency: false)
+        result = calculator.calculateRate(amount: 10)
+        XCTAssertEqual(result, "6.73")
+
+        calculator = RateCalculator(sellRate: 1.485, buyRate: 1.585, isSelling: false, isReverseUsdQuot: true, isUSDCurrency: true)
+        result = calculator.calculateRate(amount: 10)
+        XCTAssertEqual(result, "6.73")
+    }
+}
