875bccbc1d9442218c20fd2fa2df414f4b15d3d1
PavelsVetlugins <pavelvetl92@gmail.com>
2022-11-17 18:18:57 +0200

    test rename

--- Citadele-testTests/RateCalculatorTest.swift
+++ Citadele-testTests/RateCalculatorTest.swift
@@ -9,7 +9,7 @@
 import XCTest
 
 final class RateCalculatorTest: XCTestCase {
-    func testAAA() throws {
+    func testRateCalculations() throws {
         var calculator = RateCalculator(sellRate: 1.485, buyRate: 1.585, isSelling: false, isReverseUsdQuot: false, isUSDCurrency: false)
         var result = calculator.calculateRate(amount: 10)
         XCTAssertEqual(result, "15.85")
